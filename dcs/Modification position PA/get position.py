b0fd9abbbf66120219bea9d778319c5076052e90
Philippe Guilhem <philippe.guilhem@intradef.gouv.fr>
2021-02-25 00:52:07 +0100

    reprise du code

--- dcs/Modification position PA/get position.py
+++ dcs/Modification position PA/get position.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import dcs
 import configparser
 
 #On parse le fichier de configuration pour récupérer les éléments et les mettres en variables
@@ -9,14 +8,4 @@ fichierConfig = configparser.ConfigParser()
 fichierConfig.read('config.ini')
 
 # déclaration des variables 
-
-fichier = fichierConfig['mission']['nomFichier']
-idPA = fichierConfig['mission']['idPorteAvion']
-
-
-# Chargement du fichier de la mission
-mission = dcs.Mission()
-mission.load_file(fichier)
-
-
 
