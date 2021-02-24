57b08f4b89497d5bc7cc30d8353de43edf99d5dd
Philippe Guilhem <philippe.guilhem@intradef.gouv.fr>
2021-02-24 15:08:20 +0100

    Modification du fichier de script et ajout de commentaires

--- Projet mission/Position PA/get position.py
+++ Projet mission/Position PA/get position.py
@@ -2,9 +2,21 @@
 
 import dcs
 import configparser
+
+#On parse le fichier de configuration pour récupérer les éléments et les mettres en variables
+
 cfg = configparser.ConfigParser()
 cfg.read('config.ini')
-fichier = cfg['mission']['nomFichier']
 
-print(fichier)
+# déclaration des variables 
+
+fichier = cfg['mission']['nomFichier']
+idPA = cfg['mission']['idPorteAvion']
+
+
+# Chargement du fichier de la mission
+mission = dcs.Mission()
+mission.load_file(fichier)
+
+mission.ship
 
