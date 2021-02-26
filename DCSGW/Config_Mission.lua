aa1e15305528f75368c28a8a4b141965b7d10808
Salamandralex <64231662+Salamandralex@users.noreply.github.com>
2021-02-26 20:17:23 +0100

    Update Config_Mission.lua

--- DCSGW/Config_Mission.lua
+++ DCSGW/Config_Mission.lua
@@ -13,9 +13,9 @@ DCSGW_Saving_Folder_Player	 	= path_scripts.."Saves\\PlayerSave\\"
 
 -- ACTIVITY
 --================================================================================= 
-DCSGW_SSB_Activity				= true
-DCSGW_Airboss_Activity 			= true
-DCSGW_ATIS_Activity 			= true
+DCSGW_SSB_Activity				    = true
+DCSGW_Airboss_Activity 			  = true
+DCSGW_ATIS_Activity 			    = true
 
 DCSGW_AutoSaving_Activity 		= true
 
@@ -38,52 +38,52 @@ POINTS_Solde_RED			= 100000
 --================================================================================= 
 ------ Parameters => {{ Type (string) , Quantity (nb) , Cost (nb) , Lift (bool) }}  
 
-STOCK_BLUE_Vehicle_Armor_Light			= { }
-STOCK_BLUE_Vehicle_Armor_Medium			= { }
-STOCK_BLUE_Vehicle_Armor_Heavy 			= { }
-STOCK_BLUE_Vehicle_Arty		 			= { }
-STOCK_BLUE_Vehicle_AAA					= { }
-STOCK_BLUE_Vehicle_SAM_Light			= { }
-STOCK_BLUE_Vehicle_SAM_Medium			= { }
-STOCK_BLUE_Vehicle_SAM_Heavy			= { }
-STOCK_BLUE_Vehicle_Infantery 			= { }
-STOCK_BLUE_Vehicle_Logistic				= { }
+STOCK_BLUE_Vehicle_Armor_Light			 = { }
+STOCK_BLUE_Vehicle_Armor_Medium			 = { }
+STOCK_BLUE_Vehicle_Armor_Heavy 			 = { }
+STOCK_BLUE_Vehicle_Arty		 			     = { }
+STOCK_BLUE_Vehicle_AAA					     = { }
+STOCK_BLUE_Vehicle_SAM_Light			   = { }
+STOCK_BLUE_Vehicle_SAM_Medium			   = { }
+STOCK_BLUE_Vehicle_SAM_Heavy			   = { }
+STOCK_BLUE_Vehicle_Infantery 		     = { }
+STOCK_BLUE_Vehicle_Logistic				   = { }
 
-STOCK_BLUE_Airplane_Transport	 		= { }
-STOCK_BLUE_Airplane_Bombard		 		= { }
-STOCK_BLUE_Airplane_Combat		 		= { }
+STOCK_BLUE_Airplane_Transport	 		   = { }
+STOCK_BLUE_Airplane_Bombard		 		   = { }
+STOCK_BLUE_Airplane_Combat		 		   = { }
 
-STOCK_BLUE_Helico_Combat_Light			= { }
-STOCK_BLUE_Helico_Combat_Heavy			= { }
-STOCK_BLUE_Helico_Transport 			= { }
+STOCK_BLUE_Helico_Combat_Light			 = { }
+STOCK_BLUE_Helico_Combat_Heavy			 = { }
+STOCK_BLUE_Helico_Transport 			   = { }
 
-STOCK_BLUE_Special_Jtac			 		= { }
-STOCK_BLUE_Ship							= { }
-STOCK_BLUE_Ressources			 		= { }
+STOCK_BLUE_Special_Jtac			 		     = { }
+STOCK_BLUE_Ship							         = { }
+STOCK_BLUE_Ressources			 		       = { }
 
 
-STOCK_RED_Vehicle_Armor_Light			= { }
-STOCK_RED_Vehicle_Armor_Medium			= { }
-STOCK_RED_Vehicle_Armor_Heavy 			= { }
-STOCK_RED_Vehicle_Arty		 			= { }
-STOCK_RED_Vehicle_AAA					= { }
-STOCK_RED_Vehicle_SAM_Light				= { }
-STOCK_RED_Vehicle_SAM_Medium			= { }
-STOCK_RED_Vehicle_SAM_Heavy				= { }
-STOCK_RED_Vehicle_Infantery 			= { }
-STOCK_RED_Vehicle_Logistic				= { }
+STOCK_RED_Vehicle_Armor_Light			   = { }
+STOCK_RED_Vehicle_Armor_Medium			 = { }
+STOCK_RED_Vehicle_Armor_Heavy 			 = { }
+STOCK_RED_Vehicle_Arty		 			     = { }
+STOCK_RED_Vehicle_AAA					       = { }
+STOCK_RED_Vehicle_SAM_Light				   = { }
+STOCK_RED_Vehicle_SAM_Medium			   = { }
+STOCK_RED_Vehicle_SAM_Heavy				   = { }
+STOCK_RED_Vehicle_Infantery 			   = { }
+STOCK_RED_Vehicle_Logistic				   = { }
 
-STOCK_RED_Airplane_Transport	 		= { }
-STOCK_RED_Airplane_Bombard		 		= { }
-STOCK_RED_Airplane_Combat		 		= { }
+STOCK_RED_Airplane_Transport	 		   = { }
+STOCK_RED_Airplane_Bombard		 		   = { }
+STOCK_RED_Airplane_Combat		 		     = { }
 
-STOCK_RED_Helico_Combat_Light			= { }
-STOCK_RED_Helico_Combat_Heavy			= { }
-STOCK_RED_Helico_Transport 				= { }
+STOCK_RED_Helico_Combat_Light			   = { }
+STOCK_RED_Helico_Combat_Heavy			   = { }
+STOCK_RED_Helico_Transport 				   = { }
 
-STOCK_RED_Special_Jtac				 	= { }
-STOCK_RED_Ship							= { }
-STOCK_RED_Ressources				 	= { }
+STOCK_RED_Special_Jtac				 	     = { }
+STOCK_RED_Ship							         = { }
+STOCK_RED_Ressources				 	       = { }
 
 --================================================================================= 
 env.info( "-------------------------------------------------------------" )
