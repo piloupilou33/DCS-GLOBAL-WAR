f0da591277a445116582e9f443212f4c8381eb0e
Philippe Guilhem <philippe.guilhem@intradef.gouv.fr>
2021-02-26 10:29:50 +0100

    refonte du code. Parsing du LUA

--- dcs/Modification position PA/get position.py
+++ dcs/Modification position PA/get position.py
@@ -1,11 +1,24 @@
 # -*- coding: utf-8 -*-
 
 import configparser
+import luaparser
+from luaparser import ast
+import luaparser.ast
 
 #On parse le fichier de configuration pour récupérer les éléments et les mettres en variables
 
 fichierConfig = configparser.ConfigParser()
 fichierConfig.read('config.ini')
 
-# déclaration des variables 
+# Déclaration de variables
+emplacementFichier = fichierConfig['mission']['test']
+
+
+# Parsing du fichier LUA
+with open(fichierConfig['mission']['cheminFichier'], 'br') as f:
+    tree = luaparser.ast.parse(f.read().decode('u8'))
+
+for node in luaparser.ast.walk(tree):
+    print(node)
+
 
