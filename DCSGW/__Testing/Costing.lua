3c2cca9537c4100e2794c86b7c46f0985397d433
Salamandralex <64231662+Salamandralex@users.noreply.github.com>
2021-03-01 13:25:07 +0100

    Create Costing.lua

---
+++ DCSGW/__Testing/Costing.lua
@@ -0,0 +1,13 @@
+Costing_Ground = {}
+Costing_Ground["M-113"] = {}
+Costing_Ground["M-113"]["Coalition"] = "blue"
+Costing_Ground["M-113"]["Cost"] = 200
+Costing_Ground["M-113"]["item"] = "blabla"
+Costing_Ground["T90"] = {}
+Costing_Ground["T90"]["Coalition"] = "red"
+Costing_Ground["T90"]["Cost"] = 400
+Costing_Ground["T90"]["item"] = "blabla"
+Costing_Ground["T55"] = {}
+Costing_Ground["T55"]["Coalition"] = "red"
+Costing_Ground["T55"]["Cost"] = 300
+Costing_Ground["T55"]["item"] = "blabla"
