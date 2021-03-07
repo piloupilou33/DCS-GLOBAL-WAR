f04efcddd9cff1abc78bf51abf4b57b7b251a5f1
Salamandralex <64231662+Salamandralex@users.noreply.github.com>
2021-03-07 15:14:08 +0100

    update

---
+++ DCSGW/__Testing/MARKER/marker.lua
@@ -0,0 +1,19 @@
+
+
+
+-- Functions à créer
+--
+---- GLOBAL ------------------------------
+--  -create ?   [ desc = ..       ] [process = ]
+
+---- GESTION GROUPE ----------------------
+--  -select     [ desc = Selectionner les unités dans un périmètre donné  ] [process = ]
+--  -group      [ desc = Grouper les unités dans un périmètre donné       ] [process = ]
+--  -ungroup    [ desc = Degrouper les unités d'un groupe                 ] [process = ]
+
+---- TASKING -----------------------------
+--  -move       [ desc = Grouper les unités dans un périmètre donné       ] [process = ]
+--  -attack     [ desc = ..       ] [process = ]
+
+---- UTILS -------------------------------
+--  -coord      [ desc = donner des indications coordonnées à un point    ] [process = ]
