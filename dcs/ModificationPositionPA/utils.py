a0eb139e62e9599e44e9b675e020d26a9b47b9e4
argragas <avezard.jeason@gmail.com>
2021-03-09 11:58:40 +0100

    add parse_json_to_lua update to_file

--- dcs/ModificationPositionPA/utils.py
+++ dcs/ModificationPositionPA/utils.py
@@ -11,7 +11,7 @@ class Utils:
 
     def __init__(self):
         fichierConfig = configparser.ConfigParser()
-        fichierConfig.read(os.path.join(os.getcwd(),"dcs","ModificationPositionPA",'config.ini'))
+        fichierConfig.read(os.path.join(os.getcwd(), 'config.ini'))
         self.file_path = fichierConfig['mission']['CheminFichier']
 
     def parse_lua_to_json(self):
@@ -21,14 +21,21 @@ class Utils:
         code="\n".join(code)
         return lua.decode(code)
 
+    def parse_json_to_lua(self, data):
+        return lua.encode(data)
+    
     def to_file(self,data,format="json"):
         if format=="json":
             with open("output.json", "w") as json_file:
                 json.dump(data,json_file, indent=4)
+        if format=="lua":
+            with open("output_lua", "w") as lua_file:
+                lua_file.write("mission = \n")
+                lua_file.write(data)
 
     def getParam(self):
         """ Lecture du fichier csv de recherche """
-        with open(os.path.join(os.getcwd(),"dcs","ModificationPositionPA",'search.csv')) as csvfile:
+        with open(os.path.join(os.getcwd(), 'search.csv')) as csvfile:
             reader=csv.reader(csvfile)
             data = []
             for row in reader:
