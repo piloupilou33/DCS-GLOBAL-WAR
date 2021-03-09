ab846d71642ac3de335787e3b12b42dff8622a30
argragas <avezard.jeason@gmail.com>
2021-03-09 11:58:16 +0100

    clean

--- dcs/ModificationPositionPA/search.py
+++ dcs/ModificationPositionPA/search.py
@@ -1,18 +1,18 @@
 """ Classe re recherche """
 
+
 class Search:
     """ Permet de rechercher dans un JSON """
 
     def __init__(self, json_data):
         self.json_data = json_data
-
+    
     def search(self, key_list):
         """ Parcours du json en fonction de la liste de clé en parametre """
-        print(key_list)
-        if key_list is None or len(key_list) == 0:
+
+        if key_list is None or len(key_list) == 2:
             return self.json_data
         else:
-            print(type(key_list[0]))
             try:    
                 key_list[0]=int(key_list[0])
             except Exception:
@@ -20,3 +20,5 @@ class Search:
             self.json_data = self.json_data[key_list[0]]
             key_list = key_list[1:]
             return self.search(key_list)
+
+
