1025484752a44b17d124ca144f1cfadd9ffc68a2
argragas <avezard.jeason@gmail.com>
2021-03-10 11:06:24 +0100

    add zip/unzip functions

--- dcs/ModificationPositionPA/utils.py
+++ dcs/ModificationPositionPA/utils.py
@@ -4,6 +4,7 @@ import csv
 import configparser
 import os
 from slpp import slpp as lua
+from zipfile import ZipFile 
 
 
 class Utils:
@@ -13,6 +14,24 @@ class Utils:
         fichierConfig = configparser.ConfigParser()
         fichierConfig.read(os.path.join(os.getcwd(), 'config.ini'))
         self.file_path = fichierConfig['mission']['CheminFichier']
+    
+    def unzip(self):
+        """ Dezip le fichier dont le chemin est donne dans le fichier de conf """
+        # opening the zip file in READ mode 
+        with ZipFile("{}.zip".format(self.file_path), 'r') as zip: 
+            # printing all the contents of the zip file 
+            zip.printdir() 
+        
+            # extracting all the files 
+            print('Extracting all the files now...') 
+            zip.extractall() 
+            print('Done!')
+
+    def zip(self):
+        """ Zip le fichier dont le chemin est donne dans le fichier de conf """
+        with ZipFile("mission.zip",'w') as zip: 
+            zip.write("mission") 
+        print('All files zipped successfully!')   
 
     def parse_lua_to_json(self):
         """ retourne un parse json du fichier LUA donner en parametre """
@@ -29,7 +48,7 @@ class Utils:
             with open("output.json", "w", encoding="utf-8") as json_file:
                 json.dump(data,json_file, indent=4)
         if format=="lua":
-            with open("output_lua", "w", encoding="utf-8") as lua_file:
+            with open("mission", "w", encoding="utf-8") as lua_file:
                 lua_file.write("mission = \n")
                 lua_file.write(data)
 
