16f3895fb463fa69d90a55d06db573869fe6d370
Salamandralex <64231662+Salamandralex@users.noreply.github.com>
2021-03-17 00:53:53 +0100

    update

--- DCSGW/Testing/Saving/Saves/Ground_Accounts.lua
+++ DCSGW/Testing/Saving/Saves/Ground_Accounts.lua
@@ -1,5 +1,5 @@
 GroundGroupsAccounts = {}
-GroundGroupsAccounts["ID_Group_Ground_RED"] = 14
-GroundGroupsAccounts["ID_Unit_Ground_RED"] = 26
-GroundGroupsAccounts["ID_Unit_Ground_BLUE"] = 75
-GroundGroupsAccounts["ID_Group_Ground_BLUE"] = 26
+GroundGroupsAccounts["ID_Group_Ground_RED"] = 19
+GroundGroupsAccounts["ID_Unit_Ground_RED"] = 76
+GroundGroupsAccounts["ID_Unit_Ground_BLUE"] = 79
+GroundGroupsAccounts["ID_Group_Ground_BLUE"] = 27
