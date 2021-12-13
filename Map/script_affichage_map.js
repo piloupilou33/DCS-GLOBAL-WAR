90d48be61ac9af04fa9ec87f05cdad63947cfb0e
Philippe Guilhem <philippe.guilhem@intradef.gouv.fr>
2021-12-13 22:10:45 +0100

    Ajout de la popup de description des marqueurs

--- Map/script_affichage_map.js
+++ Map/script_affichage_map.js
@@ -15,11 +15,11 @@ function init() {
 
     var map = L.map('map').setView([CentreMap.latitude,CentreMap.longitude], ZoomLevel);
 
-    var calquePrincipal = L.tileLayer('https://api.maptiler.com/maps/pastel/{z}/{x}/{y}.png?key=ONO9gSCTjyyqeDhRldrV', {
+    var calquePrincipal = L.tileLayer('https://api.maptiler.com/maps/topo/{z}/{x}/{y}.png?key=ONO9gSCTjyyqeDhRldrV', {
     attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a> <a href="https://www.maptiler.com/copyright/ ">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright ">&copy; OpenStreetMap contributors</a>',
     });
 
-    // Enregistrement des icones perso
+    ///// Enregistrement des icones perso
 
    
     var ic_base = L.icon({ //icone des bases aériennes
@@ -30,44 +30,28 @@ function init() {
 
     })
 
-    // Récupération des données géo des bases à partir du fichier geoJSON
-    var baseSyrie = L.geoJSON(aeroport,{
-        pointToLayer : function (feature, latlng){
-        return L.marker(latlng, {
-            icon: ic_base
-        });
-    },
+    ///// Récupération des données géo des bases à partir du fichier geoJSON
+    var layer = L.geoJSON(aeroport, {
+        onEachFeature: function (feature, latlng) {
+            latlng.setIcon(ic_base);
+            latlng.bindPopup('<pre>'+JSON.stringify(feature.properties,null,' ').replace(/[\{\}"]/g,'')+'</pre>');
+
+        }
+       }).addTo(map);
+    
 
 
-    // todo / l'objectif est d'ajouter la popup d'information automatiquement avec les données dans le fichier JSON
+    //// Affichage de l'ensemble des calques et gestion de l'affichage
 
-        // oneachFeature: (feature = {}, layer) => {
-        //     const {properties = {} } = feature;
-        //     const { Name } = properties;
+    var overlayMaps = {
+        "Airport": layer
+        
+    };
 
-        //     if ( !Name ) return;
-
-        //     layer.bindPopup('<p>${Name}</p>');
-        // }
+    var baseMaps = {
+        "Base Map": calquePrincipal.addTo(map)
     }
-    )
-
-
-    // Affichage de l'ensemble des calques et gestion de l'affichage
-
-    var tousLesCalques = L.layerGroup([calquePrincipal, baseSyrie]);
-    tousLesCalques.addTo(map);
-
-    var controlClaques = L.control.layers({},{},{collapsed:false}).addTo(map);
-    controlClaques.addBaseLayer(calquePrincipal, 'Fond de carte');
-    controlClaques.addBaseLayer(baseSyrie, 'Base');
-
-
-
-
-
-
-
+    L.control.layers(baseMaps, overlayMaps).addTo(map);
 
 
 }
