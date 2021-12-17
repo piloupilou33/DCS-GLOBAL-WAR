c4e69999f4bd7da54aaf5687ed9d00ef6da38ac7
Papy Boyington <boyington@tamere.fr>
2021-12-17 12:11:50 +0100

    Bot opé pour les nouvelles task lisible dans les fichiers task du dossier data

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 //declaration des librairies nécessaires
 
-var lineReader = require('line-reader');
 const fs = require('fs');
+const readline = require('readline');
+const Stream = require('stream');
 const Discord = require("discord.js");
+const { readLastLines, readLastLinesEnc } = require("read-last-lines-ts")
+
 //fichier de conf du bot
 const Config = require ("./config.json");
-const prefix = "!"
+
+
 //création du client discord
 const Client = new Discord.Client({
     intents: [
         Discord.Intents.FLAGS.GUILDS,
         Discord.Intents.FLAGS.GUILD_MESSAGES
      ]});
-
-
-
 //définition du temps du bot
 var time_bot = 5;
 
@@ -33,15 +34,15 @@ const file_logistic_task_blue= "./data/logistic_task_blue.txt";
 
 //definition des ID des channels txt pour l'envoi des task dans discord
 //********ROUGE******/
-const aa_task_red_chid = '921185502890197012';
-const ag_task_red_chid = '921205276340809788';
-const csar_task_red_chid = '921205316891328572';
-const logistic_task_red_chid = '921205366879031317';
+const chid_aa_task_red = '921185502890197012';
+const chid_ag_task_red = '921205276340809788';
+const chid_csar_task_red = '921205316891328572';
+const chid_logistic_task_red = '921205366879031317';
 //********BLEU******/
-const aa_task_blue_chid = '921185541079314522';
-const ag_task_blue_chid = '921205444947636225';
-const csar_task_blue_chid = '921205496592089139';
-const logistic_task_blue_chid = '921205551235498025';
+const chid_aa_task_blue = '921185541079314522';
+const chid_ag_task_blue = '921205444947636225';
+const chid_csar_task_blue = '921205496592089139';
+const chid_logistic_task_blue = '921205551235498025';
 
 //definition des objet channel discord - seront complet lors de la connexion du bot au serveur
 //********ROUGE******/
@@ -80,22 +81,6 @@ var temp="";
 var temp2="";
 var temp3="";
 
-// //fonction de lecture de la dernière ligne d'un fichier txt
-// function read_last_ligne (file) {
-    
-//     lineReader.eachLine(file, function (line, last) {
-//         if (last) {
-//             temp=line;
-//             line="";
-//         };
-//     });
-//     if (temp!=""){
-//         return temp;
-//     } else {
-//         temp=""
-//         return temp;
-//     };
-// }; 
 
 //Connecte le client au serveur discord
 Client.login(Config.token);
@@ -105,111 +90,97 @@ Client.on("ready", () => {
     console.log("SGW Overwatch is on Air");
 
     // set les ID des channels des task de chaque coalition
-    channel_aa_task_red=Client.channels.cache.get(aa_task_red_chid);
-    channel_ag_task_red=Client.channels.cache.get(ag_task_red_chid);
-    channel_csar_task_red=Client.channels.cache.get(csar_task_red_chid);
-    channel_logistic_task_red=Client.channels.cache.get(logistic_task_red_chid);
-    channel_aa_task_blue=Client.channels.cache.get(aa_task_blue_chid);
-    channel_ag_task_blue=Client.channels.cache.get(ag_task_blue_chid);
-    channel_csar_task_blue=Client.channels.cache.get(csar_task_blue_chid);
-    channel_logistic_task_blue=Client.channels.cache.get(logistic_task_blue_chid);
+    channel_aa_task_red=Client.channels.cache.get(chid_aa_task_red);
+    channel_ag_task_red=Client.channels.cache.get(chid_ag_task_red);
+    channel_csar_task_red=Client.channels.cache.get(chid_csar_task_red);
+    channel_logistic_task_red=Client.channels.cache.get(chid_logistic_task_red);
+    channel_aa_task_blue=Client.channels.cache.get(chid_aa_task_blue);
+    channel_ag_task_blue=Client.channels.cache.get(chid_ag_task_blue);
+    channel_csar_task_blue=Client.channels.cache.get(chid_csar_task_blue);
+    channel_logistic_task_blue=Client.channels.cache.get(chid_logistic_task_blue);
 
     
 //vérifie toutes les x secondes si de nouvelles task sont apparues
     setInterval (function () {
         //Task Air/Air Rouge
-        lineReader.eachLine(file_aa_task_red, function (line, last) {
-            if (last) {
-                if (line!="" && line!=before_last_aa_task_red){
-                    channel_aa_task_red.send(line);
-                    before_last_aa_task_red=line;
-                }
-            };
-        });
-    }, time_bot * 1000);
-    delete lineReader;
-    setInterval (function () {
+        var buffer = readLastLinesEnc("utf8")(file_aa_task_red, 1)
+        if (buffer != ""){
+            if (buffer!= before_last_aa_task_red){
+            channel_aa_task_red.send("Nouvelle Task A/A Red : \n" + buffer);
+            before_last_aa_task_red=buffer;
+            }
+        }
+        buffer="";
         //Task Air/Ground Rouge
-        lineReader.eachLine(file_ag_task_red, function (line, last) {
-            if (last) {
-                if (line!="" && line!=before_last_ag_task_red){
-                    channel_ag_task_red.send(line);
-                    before_last_ag_task_red=line;
-                }
-            };
-        });
-    }, time_bot * 1000);
-          //********TASK ROUGE********** */
-        //**************************** */
-        //Task Air/Air Rouge
-        //     temp2=read_last_ligne(file_aa_task_red);
-        //     if (temp2=="" && temp2==before_last_aa_task_red){
-                
-        //     } else {
-        //         channel_aa_task_red.send(temp2);
-        //         before_last_aa_task_red=temp2;
-        //         temp2="";
-        //     }
-            
-        //     //Task Air/Ground Rouge
-        //     temp3=read_last_ligne(file_ag_task_red);
-        //     if (temp3=="" && temp3==before_last_ag_task_red){
-        //     } else {
-        //         channel_ag_task_red.send(temp3);
-        //         before_last_ag_task_red=temp3;
-        //         temp3="";
-        //     }
-        // //Task CSAR Rouge
-        // temp2=read_last_ligne(file_csar_task_red);
-        // if (temp2!=="" && temp2!==before_last_csar_task_red){
-        //     channel_csar_task_red.send(temp2);
-        //     before_last_csar_task_red=temp2;
-        // }
-        // temp2="";
-        // //Task Logistic Rouge
-        // temp2=read_last_ligne(file_logistic_task_red);
-        // if (temp2!=="" && temp2!==before_last_logistic_task_red){
-        //     channel_logistic_task_red.send(temp2);
-        //     before_last_logistic_task_red=temp2;
-        // }
-        // temp2="";
-        // //********TASK BLEU********** */
-        // //**************************** */
-        // //Task Air/Air Bleu
-        // temp2=read_last_ligne(file_aa_task_blue);
-        // if (temp2!==""&& temp2!==before_last_aa_task_blue){
-        //     channel_aa_task_blue.send(temp2);
-        //     before_last_aa_task_blue=temp2;
-        // }
-        // temp2="";
-        // //Task Air/Ground Bleu
-        // temp2=read_last_ligne(file_ag_task_blue);
-        // if (temp2!=="" && temp2!==before_last_ag_task_blue){
-        //     channel_ag_task_blue.send(temp2);
-        //     before_last_ag_task_blue=temp2;
-        // }
-        // temp2="";
-        // //Task CSAR Bleu
-        // temp2=read_last_ligne(file_csar_task_blue);
-        // if (temp2!=="" && temp2!==before_last_csar_task_blue){
-        //     channel_csar_task_blue.send(temp2);
-        //     before_last_csar_task_blue=temp2;
-        // }
-        // temp2="";
-        // //Task Logistic Bleu
-        // temp2=read_last_ligne(file_logistic_task_blue);
-        // if (temp2!=="" && temp2!==before_last_logistic_task_blue){
-        //     channel_logistic_task_blue.send(temp2);
-        //     before_last_logistic_task_blue=temp2;
-        // }
-        // temp2="";
-        // }, time_bot * 1000)
+        buffer = readLastLinesEnc("utf8")(file_ag_task_red, 1)
+        if (buffer != ""){
+            if (buffer!= before_last_ag_task_red){
+                before_last_ag_task_red=buffer;
+                channel_ag_task_red.send("Nouvelle Task A/G Red : \n" + buffer);
+            }
+        }
+        buffer=""; 
+        //Task CSAR Rouge
+        buffer = readLastLinesEnc("utf8")(file_csar_task_red, 1)
+        if (buffer != ""){
+            if (buffer!= before_last_csar_task_red){
+                before_last_csar_task_red=buffer;
+                channel_csar_task_red.send("Nouvelle Task CSAR Red : \n" + buffer);
+            }
+        }
+        buffer="";
+        //Task Logistic Rouge
+        buffer = readLastLinesEnc("utf8")(file_logistic_task_red, 1)
+        if (buffer != ""){
+            if (buffer!= before_last_logistic_task_red){
+                before_last_logistic_task_red=buffer;
+                channel_logistic_task_red.send("Nouvelle Task Logistic Red : \n" + buffer);
+            }
+        }
+        buffer=""; 
+        //Task A/A Blue
+        buffer = readLastLinesEnc("utf8")(file_aa_task_blue, 1)
+        if (buffer != ""){
+            if (buffer!= before_last_aa_task_blue){
+                before_last_aa_task_blue=buffer;
+                channel_aa_task_blue.send("Nouvelle Task A/A Blue : \n" + buffer);
+            }
+        }
+        buffer=""; 
+        //Task A/G Blue
+        buffer = readLastLinesEnc("utf8")(file_ag_task_blue, 1)
+        if (buffer != ""){
+            if (buffer!= before_last_ag_task_blue){
+                before_last_ag_task_blue=buffer;
+                channel_ag_task_blue.send("Nouvelle Task A/G Blue : \n" + buffer);
+            }
+        }
+        buffer=""; 
+        //Task CSAR Blue
+        buffer = readLastLinesEnc("utf8")(file_csar_task_blue, 1)
+        if (buffer != ""){
+            if (buffer!= before_last_csar_task_blue){
+                before_last_csar_task_blue=buffer;
+                channel_csar_task_blue.send("Nouvelle Task CSAR Blue : \n" + buffer);
+            }
+        }
+        buffer=""; 
+        //Task Logistic Blue
+        buffer = readLastLinesEnc("utf8")(file_logistic_task_blue, 1)
+        if (buffer != ""){
+            if (buffer!= before_last_logistic_task_blue){
+                before_last_logistic_task_blue=buffer;
+                channel_logistic_task_blue.send("Nouvelle Task Logistic Blue : \n" + buffer);
+            }
+        }
+        buffer=""; 
 
+    }, time_bot * 1000);
 });
 
 // Fonction !clearchat qui permet de clear 99 messages d'un channel Discord
 Client.on("messageCreate", msg => {
-    if (msg.content.toLowerCase().startsWith(prefix + "clearchat")) {
+    if (msg.content.toLowerCase().startsWith(Config.prefix + "clearchat")) {
         msg.channel.bulkDelete(99);
     }
 });
